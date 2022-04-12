e22bf3851d44d8a10c0dd2771a8142674b4f5813
osondu dominion <domikdegreat@gmail.com>
2022-04-12 08:24:08 -0700

    for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * A program that will print _putchar and followed by a new line.
+ *
+ * _putchar(character)
+ * 
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
