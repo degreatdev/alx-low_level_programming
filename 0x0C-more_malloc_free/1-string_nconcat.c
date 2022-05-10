d3defd986a3878e182bc1ba700e218faae95e92d
osondu dominion <domikdegreat@gmail.com>
2022-05-09 19:19:22 -0700

    for more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,57 +1,50 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * string_nconcat -  function that concatenates two strings.
- *
- * @s1: string 1
- * @s2: string 2
- * @n: n bytes
- * Return: pointer.
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *p;
-unsigned int i, j, len1 = 0, len2 = 0;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
-while (s1 && s1[len1])
-len1++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-while (s2 && s2[len2])
-len2++;
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
 
-if (n >= len2)
-p = malloc(sizeof(*s1) * len1 + (len2 + 1));
-else
-p = malloc(sizeof(*s1) * len1 + (n + 1));
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 
-if (p == NULL)
-return (NULL);
+	if (p == NULL)
+		return (0);
 
-i = 0;
-while (i < len1)
-{
-p[i] = s1[i];
-i++;
-}
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
 
-j = 0;
-while (n < len2 && i < (len1 + n))
-{
-p[i] = s2[j];
-i++;
-j++;
-}
-while (n >= len2 && i < (len1 + len2))
-{
-p[i] = s2[j];
-i++;
-j++;
-}
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
 
-p[i] = '\0';
 return (p);
 }
