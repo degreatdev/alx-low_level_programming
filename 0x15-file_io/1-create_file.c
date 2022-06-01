ae2c38958751b5d22f5ad688df405bd5f5855328
Osondu Dominion <domikdegreat@gmail.com>
2022-06-02 00:57:26 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,43 +6,32 @@
  * @text_content: a NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-/**
-* create_file - check the code for Holberton School students.
-* @filename: file to create.
-* @text_content: info to write into the file.
-* Return: 1 on success, -1 on failure
-*/
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t nletters;
-	int file;
+int fd;
+int nletters;
+int rwr;
 
-	if (!filename)
-		return (1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
-	{
-		return (-1);
-	}
-	if (text_content)
-	{
-		nletters = write(file, text_content, _strlen(text_content));
-		if (nletters == -1)
-		{
-			close(file);
-			return (-1);
-		}
-	}
-	close(file);
-	return (1);
+if (!filename)
+return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+if (fd == -1)
+return (-1);
+
+if (!text_content)
+text_content = "";
+
+for (nletters = 0; text_content[nletters]; nletters++)
+;
+
+rwr = write(fd, text_content, nletters);
+
+if (rwr == -1)
+return (-1);
+
+close(fd);
+
+return (1);
 }
